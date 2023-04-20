03d4f20d3027636336fe434d3023626d6cd44c9c
carloshkruger <carloshkruger@gmail.com>
2023-04-20 14:53:56 -0300

    removing unnecessary folder

--- kinesis-lambda/.aws-sam/build/ConsumeKinesisStreamFunction/src/handlers/consume-kinesis-stream.js
+++
@@ -1,9 +0,0 @@
-export const handler = async (event) => {
-  for (const record of event.Records) {
-    console.log(record);
-    const payload = Buffer.from(record.kinesis.data, "base64").toString(
-      "ascii"
-    );
-    console.log("Decoded payload:", payload);
-  }
-};
